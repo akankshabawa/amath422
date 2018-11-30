c6ae47b389efe73e5918e7dd0f434def63d6242a
paschalmj <45110972+paschalmj@users.noreply.github.com>
2018-11-29 21:52:24 -0800

    Add files via upload

--- final_422.m
+++ final_422.m
@@ -24,26 +24,32 @@ left_index = (D/2 - L/2 + init_patch_center)/dx + 1; %index of x where habitat s
 right_index = left_index + L/dx; %index of x where habitat ends
 %size(y) = 1x201
 %size(x) = 1x2401
-n0 = init_dist(init_sigma, init_patch_center, x, y); %Initial distribution
+n0 = init_dist(init_sigma, init_patch_center, x, k); %Initial distribution
 
 %RUN DETERMINISTIC MODEL
 burn = 10; %Number of iterations to get equilibrium before adding climate change
 param = 1;
 for i = 1:burn
     patch_n0 = n0(left_index:right_index); %previous densities in habitat
-    K = outerFunc(x,y,dispersal_type, 1);
-    newn = dx*K*bev_holt(patch_n0, R, k);
+    K = outerFunc(x,y,dispersal_type, param);
+    size(K)
+    size(patch_n0)
+    blah = bev_holt(patch_n0, R, k);
+    size(blah)
+    newn = dx.*K*blah.';
+    newn = newn.';
     n0 = newn;
-    y = x(left_index: right_index);
-    %plot(patch_n0); hold on;
+    y = x(left_index:right_index);
+    
 end
-
+plot(n0)
 for t = 2:N
           patch_n0 = n0(left_index:right_index);
           K = outerFunc(x,y,dispersal_type, param);         
-          newn = dx*K*bev_holt(patch_n0, R, k);
+          newn = dx*K*bev_holt(patch_n0, R, k).';
+          newn = newn.';
           %plot new population density newn in grey
-          plot(newn)
+          plot(newn); hold on;
           %end simulation if population is -->0
           if(trapz(x,newn)< 5)
           break  
@@ -62,9 +68,9 @@ end
 patch_n0;
 
 function outer = outerFunc(x,y, dispersal_type, param)
-    outer = zeros(2401, 201);
-    for i = 1 : 2401
-        for j = 1 : 201
+    outer = zeros(length(x), length(y));
+    for i = 1 : length(x)
+        for j = 1 : length(y)
             outer(i,j) = kernel(x(i),y(j),dispersal_type, param);
         end
     end
@@ -86,13 +92,10 @@ end
 
 %GROWTH FUNCTION (BEVERTON-HOLT HERE)
 function growth = bev_holt(patch_n0, R, k)
-    growth = (R*patch_n0 / (1 + (R-1)/k*patch_n0));
+    growth = (R.*patch_n0 ./ (1 + (R-1)/k.*patch_n0));
 end
 
 %INITIAL DISTRIBUTION FUNCTION (USES GAUSSIAN KERNEL)
-function n0 = init_dist(init_sigma, init_patch_center, x, y)
-    n0 = 100/(sqrt(2*pi)*init_sigma)*exp(-(x-(init_patch_center+0)).^2/(2*init_sigma.^2));
+function n0 = init_dist(init_sigma, init_patch_center, x, k)
+    n0 = k/(sqrt(2*pi)*init_sigma)*exp(-(x-(init_patch_center+0)).^2/(2*init_sigma.^2));
 end
-
-
-
